038eaa66ae47e93477c4d0dc86dc1c3b0cd07556
Matthijs4004 <matthijs4004@gmail.com>
2021-12-15 11:13:29 +0100

    fininshed lightswitch assignment

--- lightswitch.py
+++ lightswitch.py
@@ -1,13 +1,24 @@
 import tkinter as tk
+
 window = tk.Tk()
 
-button = tk.Button(text='...', bg="white", fg="black")
+# schijf hier tussen je code
+
+def lightswitch():
+    if button["text"] == "lightswitch off":
+        button["text"] = "lightswitch on"
+        window['background']='yellow'
+        print("light is on")
+    else:
+        button["text"] = "lightswitch off"
+        window['background']='black'
+        print("light is off")
+
+# schijf hier tussen je code
+
+button = tk.Button(text='lightswitch off', bg="white", fg="black", command=lightswitch)
 button.pack(pady = 20, padx = 20)
+window['background']='black'
 
-# schijf hier tussen je code
-
-
-
-# schijf hier tussen je code
 
 window.mainloop()
